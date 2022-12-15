ff36cdb95d0e3da26efa84794eb1af14d8036f72
Selvi Ece Dugan <selvidugan@gmail.com>
2022-12-15 10:18:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(userName, userEmail) {
+
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            "Accept": 'application/json'
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    }).then(response => response.json())
+        .then(data => {
+            document.querySelector("body").append(data.id)
+        })
+        .catch(err => {
+
+            document.querySelector("body").innerHTML = err.message
+            console.log(document.querySelector("body").innerHTML)
+        }
+        )
+
+}
+
